e4c2c7f6dca56e1fc311c542fd143961d1082e5a
Jpower3 <61142002+Jpower3@users.noreply.github.com>
2022-02-08 16:19:20 -0800

    Updated visuals
    Added timer into the challenge

--- CookingPapa.py
+++ CookingPapa.py
@@ -2,8 +2,8 @@ from email import message
 from pynput import keyboard
 import time as t 
 import paho.mqtt.client as mqtt
-from threading import Thread
-import sys
+import threading
+import ctypes
 ##CONST GLOBALS
 
 GOAL_STOVE = 1
@@ -36,13 +36,43 @@ flag_received = 0
 flag_score = 3
 score = 0
 message_received = ''
+
 #globals
 
+class thread_with_exception(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
+             
+    def run(self):
+ 
+        # target function of the thread class
+        try:
+            i = 0
+            while(True):
+                i = i+1
+                t.sleep(1)
+                print("                           "+str(i), end="\r")
+        finally:
+            print('ended')
+          
+    def get_id(self):
+ 
+        # returns id of the respective thread
+        if hasattr(self, '_thread_id'):
+            return self._thread_id
+        for id, thread in threading._active.items():
+            if thread is self:
+                return id
+  
+    def raise_exception(self):
+        thread_id = self.get_id()
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
+              ctypes.py_object(SystemExit))
 #mqtt interaction
 def on_connect(client, userdata, flags, rc):
     global flag_player
     global flag_opponent
-    global flag_received
     print("Connection returned result: "+str(rc))
     
 # Subscribing in on_connect() means that if we lose the connection and
@@ -137,19 +167,19 @@ def on_press(key):
                 #timing of spins
                 if diff < IDEAL_SPIN * 1.1 and diff > IDEAL_SPIN * 0.9:
                     spins = spins + 1.5
-                    print("Good job! Perfect Spin!")
+                    print("Good job! Perfect Spin!                              ")
                 elif diff < IDEAL_SPIN * 1.2 and diff > IDEAL_SPIN * 0.8:
                     spins = spins + 1
-                    print("Okay job! Great Spin!")
+                    print("Okay job! Great Spin!                                ")
                 elif diff < IDEAL_SPIN * 2 and diff > IDEAL_SPIN * 0.5:
                     spins = spins + 0.2
                     print("That was not a great spin, try to equalize your pace!")
                 else:
-                    print("You're an idiot sandwich. Try again.")
+                    print("You're an idiot sandwich. Try again.                 ")
                 print(spins)
                 return False
             else:
-                print('wrong key')
+                print("wrong key                                                ")
                 return False
 
     #given cooking = 1, cutting      
@@ -166,22 +196,22 @@ def on_press(key):
                 #timing of cut
                 if diff < IDEAL_CUT * 1.1 and diff > IDEAL_CUT * 0.9:
                     chops = chops + 1.5
-                    print("Good job! Perfect Cut!")
+                    print("Good job! Perfect Cut!                              ")
                 elif diff < IDEAL_CUT * 1.2 and diff > IDEAL_CUT * 0.8:
                     chops = chops + 1
-                    print("Okay job! Great Cut!")
+                    print("Okay job! Great Cut!                                ")
                 elif diff < IDEAL_CUT * 2 and diff > IDEAL_CUT * 0.5:
                     chops = chops + 0.2
                     print("That was not a great cut, try to equalize your pace!")
                 else:
-                    print("You're an idiot sandwich. Try again.")
+                    print("You're an idiot sandwich. Try again.                ")
                 print(chops)
                 return False
             else:
-                print('wrong key')
+                print("wrong key                                               ")
                 return False
         else:
-            print('wrong key')
+            print("wrong key                                                   ")
             return False
 #
 #GAME STARTS HERE
@@ -198,14 +228,14 @@ client.on_disconnect = on_disconnect
 client.on_message = on_message
 # 2. connect to a broker using one of the connect*() functions.
 client.connect_async("test.mosquitto.org")
-# 3. call one of the loop*() functions to maintain network traffic flow with the broker.
 client.loop_start()
-def timer():
-    for i in range (108):
-        t.sleep(1)
-    sys.exit()
 def main():
     global score
+    global in_cooking
+    global spins
+    global chops
+    global total_cutting
+    global total_stove
     #wait for player selection
     while(flag_player==0):
         pass
@@ -218,7 +248,7 @@ def main():
     print('Your opponent is in the lobby')
     client.publish(str(flag_opponent)+'Team8','1gamestart',qos=1)
     t.sleep(2)
-    txt = input('Type Ready to begin: ')
+    txt = input('Type Ready to begin: \n')
     if txt.lower() == 'ready':
         client.publish(str(flag_opponent)+'Team8', '2I am Ready',qos=1)
         print('Waiting for your opponent to be ready!')
@@ -232,8 +262,10 @@ def main():
             listener.join()
         in_cooking = 1
         if position == STOVE:
-            txt = input('Type spoon to start stirring: ')
+            txt = input('Type spoon to start stirring: \n')
             if txt == 'spoon':
+                t2 = thread_with_exception('timer')
+                t2.start()
                 print('Press up right down left chef')
                 while(spins<4):
                     with keyboard.Listener(on_press=on_press)as listener:
@@ -241,9 +273,13 @@ def main():
                 spins = 0
                 total_stove = total_stove + 1
                 print("Total stove times remaining: "+ str(GOAL_STOVE-total_stove))
+                t2.raise_exception()
+                t2.join()
         elif position == CUTTING:
-            txt = input('Type knife to start chopping: ')
+            txt = input('Type knife to start chopping: \n')
             if txt == 'knife':
+                t2 = thread_with_exception('timer')
+                t2.start()
                 print('Press up and down chef')
                 while(chops<6):
                     with keyboard.Listener(on_press=on_press)as listener:
@@ -251,6 +287,8 @@ def main():
                 chops = 0
                 total_cutting = total_cutting + 1
                 print("Total cutting times remaining: "+ str(GOAL_CUTTING-total_cutting))
+                t2.raise_exception()
+                t2.join()
         in_cooking = 0
         if total_cutting >= GOAL_CUTTING and total_stove >= GOAL_STOVE:
             in_cooking = 2
@@ -258,12 +296,9 @@ def main():
     end_game = t.time()
     score = end_game-start_game
     print('Your time was: ' + str(score))
-    client.publish(str(flag_score)+str(flag_opponent)+'Team8', float(score), qos=1)
+    client.publish(str(flag_opponent)+'Team8', str(flag_score)+str(score), qos=1)
     print("waiting for opponent's time...")
     while True:
         pass
-
-t1 = Thread(target = main)
-t2 = Thread(target = timer)
+t1 = threading.Thread(target = main)
 t1.start()
-t2.start()
